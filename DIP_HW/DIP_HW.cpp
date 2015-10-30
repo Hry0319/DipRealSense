37a217f8b88c080299de060988e5f80e8b3dd381
JayHsieh <k3331863@gmail.com>
2015-10-30 17:22:41 +0800

    Opencv 3.0.0 + RealSense SDK environment initial

---
+++ DIP_HW/DIP_HW.cpp
@@ -0,0 +1,11 @@
+// DIP_HW.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "opencv2/opencv.hpp"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
